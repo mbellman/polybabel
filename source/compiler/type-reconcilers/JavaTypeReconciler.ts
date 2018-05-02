689889b5859c25009e1e7024cc3f4b205f0dc1ad
mbellman <mbellman@nwe.com>
2018-05-02 01:44:09 -0500

    Mistyped import path fix

--- source/compiler/type-reconcilers/JavaTypeReconciler.ts
+++ source/compiler/type-reconcilers/JavaTypeReconciler.ts
@@ -1,7 +1,7 @@
 import AbstractTypeReconciler from '../common/AbstractTypeReconciler';
 import { Implements } from 'trampoline-framework';
 import { IReconciledType } from '../common/compiler-types';
-import { JavaSyntax } from 'parser/java/java-syntax';
+import { JavaSyntax } from '../../parser/java/java-syntax';
 
 export default class JavaTypeReconciler extends AbstractTypeReconciler {
   @Implements public reconcile (javaSyntaxTree: JavaSyntax.IJavaSyntaxTree): IReconciledType[] {
